c6a1f0e0c116fbe4773104b1da256ad1a3845c0b
gustavogurg <gustavogurgel@edu.unifor.br>
2026-06-04 18:29:02 -0300

    video e fix rotas

--- src/routes/stockMovementRoutes.js
+++ src/routes/stockMovementRoutes.js
@@ -4,10 +4,11 @@ import stockMovementController from "../controllers/stockMovementController.js";
 const router = Router();
 
 router.get("/", stockMovementController.index);
-router.get("/:id", stockMovementController.show);
-
+router.get("/product/:productId", stockMovementController.findByProductId); // rota faltou
 router.get("/:id", stockMovementController.show);
 
 router.post("/", stockMovementController.store);
+// router.put("/:id", stockMovementController.update);
+// router.delete("/:id", stockMovementController.destroy);
 
 export default router;
